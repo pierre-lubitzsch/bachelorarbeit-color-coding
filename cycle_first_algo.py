507e56dc72efafc3876a975c1fee273be3f4d817
Pierre Lubitzsch <pierre.lubitzsch@web.de>
2021-07-26 14:03:58 +0200

    One test graph with image and algorithms for finding a path and a cycle

---
+++ cycle_first_algo.py
@@ -0,0 +1,121 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import math
+import random
+
+
+
+def find_colorful_paths_from_node(G: nx.graph.Graph, k: int, c: dict, start_vertex: int) -> [()]:
+    paths = set()
+    dp = dict()
+    pred = dict()
+
+    for v in G.nodes:
+        dp[v] = [set() for _ in range(k)]
+        pred[v] = dict()
+    
+    dp[start_vertex][0].add(frozenset([c[start_vertex]]))
+    pred[start_vertex][frozenset([c[start_vertex]])] = ()
+
+    for length in range(k - 1):
+        for v in G.nodes:
+            for colorset in dp[v][length]:
+                for u in G.adj[v]:
+                    if c[u] not in colorset:
+                        new_colorset = set(colorset)
+                        new_colorset.add(c[u])
+                        const_set = frozenset(new_colorset)
+                        dp[u][length + 1].add(const_set)
+                        pred[u][const_set] = pred[v][colorset] + (v,)
+
+    for v in G.nodes:
+        for colorset in dp[v][k - 1]:
+            paths.add(pred[v][colorset] + (v,))
+    
+    return paths
+
+
+
+def find_paths(G: nx.graph.Graph, k: int, start_vertex: int) -> [()]:
+    if (k > len(list(G.nodes))):
+        return ()
+
+    paths = set()
+    c = dict()
+
+    # get a random coloring e^k times and execute the coloring algorithm with it
+    for _ in range(math.ceil(pow(math.e, k))):
+        for v in G.nodes:
+            # assign uniformly random value to coloring
+            c[v] = random.randint(1, k)
+        
+        cur_paths = find_colorful_paths_from_node(G, k, c, start_vertex)
+        for path in cur_paths:
+            paths.add(path)
+    
+    return list(paths)
+
+
+def find_cycle(G: nx.graph.Graph, k: int) -> ():
+    if (k <= 2 or k > len(list(G.nodes))):
+        return ()
+
+    for v in G.nodes:
+        paths = find_paths(G, k, v)
+
+        """
+        path_ends = dict()
+        for (i, path) in enumerate(paths):
+            last = path[-1] if v != path[-1] else path[0]
+            path_ends[last] = i
+        """
+
+        for path in paths:
+            if (G.has_edge(path[0], path[-1]) and test_path_length_k(G, path, k)):
+                cycle = path + (path[0],)
+                return cycle
+        
+    return ()
+
+
+def test_path_length_k(G: nx.graph.Graph, path : [], k: int):
+    if (len(path) != k):
+        return False
+    for i in range(len(path) - 1):
+        if (not G.has_edge(path[i], path[i + 1])):
+            return False
+    return True
+
+
+def test_cycle_length_k(G: nx.graph.Graph, cycle : [], k: int):
+    if (len(cycle) != k + 1):
+        return False
+    for i in range(len(path) - 1):
+        if (not G.has_edge(cycle[i], cycle[i + 1])):
+            return False
+    return True
+
+
+def test():
+    G = nx.Graph()
+    with open("nodes.txt", 'r') as f:
+        for v in f:
+            if (v == ''):
+                continue
+            G.add_node(int(v))
+
+    with open("edges.txt", 'r') as f:
+        for line in f:
+            (u, v, w) = line.split(';')
+            G.add_edge(int(u), int(v), weight=float(w))
+
+    #T = prim(G, len(list(G.nodes)))
+
+    for i in range(1,12):
+        print("Does G contain C_{}? {}".format(i, find_cycle(G, i)))
+
+
+
+
+if(__name__ == "__main__"):
+    test()
---
+++ cycle_second_algo.py
@@ -0,0 +1,137 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import math
+import random
+import numpy as np
+import itertools
+
+
+"""
+original_G is the graph we had at the first call of this function
+colors are the colors we are currently using
+"""
+def find_colorful_paths(G: nx.graph.Graph, k: int, c: dict, original_G: nx.graph.Graph, colors: []):
+    n = len(list(G.nodes))
+    if (k == 1):
+        B = np.identity(n)
+        for v in original_G.nodes:
+            if v not in G.nodes:
+                B[v][v] = False
+        return B
+    if (k == 2):
+        # return adjacency matrix of G
+        B = nx.to_numpy_array(G)
+        return B
+    """
+    if (k == 2):
+        B = np.zeros((n, n))
+        for u in G.nodes:
+            for w in G.nodes:
+                if (c[u] == c[w]):
+                    continue
+                for v in G.adj[u]:
+                    if (c[u] != c[v] and c[v] != c[w] and v in G.adj[w]):
+                        B[u][w] = B[w][u] = 1
+        return B
+    """
+
+    paths = np.zeros((n, n))
+    # get all subsets of {1, ..., k} having size k // 2
+    subsets = set(itertools.combinations(colors, len(colors) // 2))
+
+    for c_1 in subsets:
+        c_2 = set((i for i in colors if i not in c_1))
+
+        V_1 = [v for v in G.nodes if c[v] in c_1]
+        V_2 = [v for v in G.nodes if c[v] in c_2]
+
+        G_1 = G.copy()
+        G_2 = G.copy()
+
+        for e in G_1.edges:
+            if (e[0] not in V_1 or e[1] not in V_1):
+                G_1.remove_edge(e[0], e[1])
+        for e in G_2.edges:
+            if (e[0] not in V_2 or e[1] not in V_2):
+                G_2.remove_edge(e[0], e[1])
+
+        A_1 = find_colorful_paths(G_1, k // 2, c, original_G, c_1)
+        A_2 = find_colorful_paths(G_2, (k + 1) // 2, c, original_G, c_2) # (k + 1) // 2 is k/2 rounded up
+
+        # adjacency matrix between nodes of V_1 and V_2
+        B = np.zeros((n, n))
+        for i in V_1:
+            for j in V_2:
+                if (original_G.has_edge(i, j)):
+                    B[i][j] = B[j][i] = 1
+
+        #print("At k = {}".format(k))
+        #print("Got from {}:\n{}".format(V_1, A_1))
+        #print("Got from {}:\n{}".format(V_2, A_2))
+        #print("Adjacency between {} and {}:\n{}".format(V_1, V_2, B))
+
+        cur_paths = np.dot(np.dot(A_1, B), A_2)
+
+        #print("Result for this time:\n{}".format(cur_paths))
+        #print("\n\n\n")
+
+        paths = np.logical_or(paths, cur_paths)
+
+    return paths
+
+
+def find_cycle(G: nx.graph.Graph, k: int) -> bool:
+    if (k <= 2 or k > len(list(G.nodes))):
+        return False
+
+    c = dict()
+    n = len(list(G.nodes))
+
+    # get a random coloring e^k times and execute the coloring algorithm with it
+    for _ in range(math.ceil(pow(math.e, k))):
+        for v in G.nodes:
+            # assign uniformly random value to coloring
+            c[v] = random.randint(1, k)
+        
+        cycles = find_colorful_paths(G, k, c, G, list(range(1, k + 1)))
+        for i in range(n):
+            for j in range(n):
+                if ((cycles[i][j] or cycles[j][i]) and G.has_edge(i, j)):
+                    return (i, j)
+                    return True    
+        
+    return False
+
+
+def test():
+    G = nx.Graph()
+    with open("nodes.txt", 'r') as f:
+        for v in f:
+            if (v == ''):
+                continue
+            G.add_node(int(v))
+
+    with open("edges.txt", 'r') as f:
+        for line in f:
+            (u, v, w) = line.split(';')
+            G.add_edge(int(u), int(v))
+
+    #T = prim(G, len(list(G.nodes)))
+
+    for i in range(1,11):
+        print("Does G contain C_{}? {}".format(i, find_cycle(G, i)))
+
+if(__name__ == "__main__"):
+    test()
+
+
+
+"""
+print(A_1)
+print(A_2)
+print(B)
+print(type(A_1))
+print(type(A_2))
+print(type(B))
+return cycles
+"""
